cfda3f35c3bb85d7b7cd5540423d39b5a5003b79
Annie Ausen <ausen.annie@gmail.com>
2021-07-12 14:49:16 -0500

    solution

--- reverse.py
+++ reverse.py
@@ -42,6 +42,19 @@ def rev_string(astring):
         new_lst.append(end_char)
         rev_string("".join(new_lst))
 
+def rev_string(astring):
+    """Return reverse of string using recursion.
+
+    You may NOT use the reversed() function!
+    """
+
+    # START SOLUTION
+
+    if len(astring) < 2:
+        return astring
+
+    return astring[-1] + rev_string(astring[:-1])
+
 # if __name__ == '__main__':
 #     import doctest
 #     if doctest.testmod().failed == 0:
