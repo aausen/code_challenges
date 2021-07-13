be97a3a58322b44aa39395143ce9a813dacc471a
Annie Ausen <ausen.annie@gmail.com>
2021-07-13 12:22:45 -0500

    working on find_num_sum

--- aoc_day1.py
+++ aoc_day1.py
@@ -1,9 +1,8 @@
 def find_num_sum():
 
     nums = open("aoc_day1.txt")
-
     for i, num in enumerate(nums):
-        
-        if num[i] + num[i + 1] == 2020:
-            return num[i]*num[i + 1]
-        
+        for j, num2 in enumerate(nums(i + 1)):
+            if num + num2 == 2020:
+                return num*num2
+            
