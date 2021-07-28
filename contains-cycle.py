a132e00c014e6f9d76ffc6bf200ec9a2b403a7ff
Annie Ausen <ausen.annie@gmail.com>
2021-07-28 12:08:54 -0500

    finish challenge with tests

--- contains-cycle.py
+++ contains-cycle.py
@@ -1,9 +1,9 @@
 import unittest
 
-def contains_cycle(head):
+def contains_cycle(first_node):
     # looks through each node in the ll
     # set current node
-    current = head
+    current = first_node
     # keep track of nodes we have seen in set
     seen = set()
     while current:
@@ -11,12 +11,12 @@ def contains_cycle(head):
         if current in seen:
             return True
         # if the current node is none, we hit the end so return False
-        elif current == None:
+        elif current.next == None:
             return False
         # else add the current node to our set and keep looping
         else:
             seen.add(current)
-            current = head.next
+            current = current.next
 
 # Tests
 class Test(unittest.TestCase):
@@ -51,6 +51,31 @@ class Test(unittest.TestCase):
         second = Test.LinkedListNode(2, third)
         first = Test.LinkedListNode(1, second)
         fifth.next = third
+        result = contains_cycle(first)
+        self.assertTrue(result)
+
+    def test_two_node_cycle_at_end(self):
+        fifth = Test.LinkedListNode(5)
+        fourth = Test.LinkedListNode(4, fifth)
+        third = Test.LinkedListNode(3, fourth)
+        second = Test.LinkedListNode(2, third)
+        first = Test.LinkedListNode(1, second)
+        fifth.next = fourth
+        result = contains_cycle(first)
+        self.assertTrue(result)
+
+    def test_empty_list(self):
+        result = contains_cycle(None)
+        self.assertFalse(result)
+
+    def test_one_element_linked_list_no_cycle(self):
+        first = Test.LinkedListNode(1)
+        result = contains_cycle(first)
+        self.assertFalse(result)
+
+    def test_one_element_linked_list_cycle(self):
+        first = Test.LinkedListNode(1)
+        first.next = first
         result = contains_cycle(first)
         self.assertTrue(result)
 
