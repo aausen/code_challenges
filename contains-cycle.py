afbc403a6b43269d4c749a690015c88d903cee1f
Annie Ausen <ausen.annie@gmail.com>
2021-07-28 11:29:46 -0500

    pseudocode

---
+++ contains-cycle.py
@@ -0,0 +1,7 @@
+def contains_cycle(head):
+    # looks through each node in the ll
+    # set current node
+    # keep track of nodes we have seen in set
+    # if we see node again, return True
+    # if the current node is none, we hit the end so return False
+    # else add the current node to our set and keep looping
