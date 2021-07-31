5bf4f056db3c4c5db8064a813dd666b16544027c
Annie Ausen <ausen.annie@gmail.com>
2021-07-31 15:38:14 -0500

    added tests

--- HiCal.py
+++ HiCal.py
@@ -1,3 +1,5 @@
+import unittest
+
 def merge_ranges(time_range):
    
     sorted_times = sorted(time_range)
@@ -16,8 +18,40 @@ def merge_ranges(time_range):
             merged_meetings.append((current_meeting_start, current_meeting_end))
 
     return merged_meetings
-    
-    
 
+    
+# Tests
 
-merge_ranges([(1,3), (2, 4), (7, 9), (3, 10)])
+class Test(unittest.TestCase):
+
+    def test_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (2, 4)])
+        expected = [(1, 4)]
+        self.assertEqual(actual, expected)
+
+    def test_no_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (4, 6)])
+        expected = [(1, 3), (4, 6)]
+        self.assertEqual(actual, expected)
+    
+    def test_all_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (3, 7), (5, 10)])
+        expected = [(1, 10)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_touch(self):
+        actual = merge_ranges([(1, 3), (3, 5)])
+        expected = [(1, 5)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_not_sorted(self):
+        actual = merge_ranges([(3, 5), (4, 6), (1, 2)])
+        expected = [(1, 2), (3, 6)]
+        self.assertEqual(actual, expected)
+
+    def test_contains_other_meetings(self):
+        actual = merge_ranges([(1, 8), (3, 4)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+unittest.main(verbosity=2)
