d06492ff66ef0e97b6d2233470ab24a0d3d646ba
Annie Ausen <ausen.annie@gmail.com>
2021-07-31 15:21:10 -0500

    pseudocode for challenge

--- HiCal.py
+++ HiCal.py
@@ -1,17 +1,22 @@
 def merge_ranges(time_range):
-    # sort the list by start times
-    print("times: ", time_range)
+   
     sorted_times = sorted(time_range)
-    print("sorted", sorted_times)
-    # first = meeting with earlier start time
-    # second = meeting with later start time
-    # if the end time of the first meeting is
-    #   greater thank or equal to the start time of the 
-    #   second meeting, merge the two meetings
-    #   the result will be the first meeting's start time
-    #   and the end time will be the later of the two meeting's end times
-    # else we leave them seperate
-    # return a merged list of times
+    
+    merged_meetings = [sorted_times[0]]
+    
+    for current_meeting_start, current_meeting_end in sorted_times[1:]:
+        last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
+
+        if (current_meeting_start <= last_merged_meeting_end):
+            merged_meetings[-1] = (last_merged_meeting_start, 
+                                    max(last_merged_meeting_end, 
+                                        current_meeting_end))
+
+        else: 
+            merged_meetings.append((current_meeting_start, current_meeting_end))
+
+    return merged_meetings
+    
 
 
 merge_ranges([(1,3), (2, 4), (7, 9), (3, 10)])
