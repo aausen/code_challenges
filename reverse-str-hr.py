0db246b15a2d06b23da1d98e6bd485a42e4d99da
Annie Ausen <ausen.annie@gmail.com>
2021-08-02 15:33:32 -0500

    work with reversed

---
+++ reverse-str-hr.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    
+    reversed_arry = arr.reverse()
+    print(reversed_arry)
