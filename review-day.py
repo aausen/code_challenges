ac34c243a7f6870eb6edd94878dcb59c768a1062
Annie Ausen <ausen.annie@gmail.com>
2021-08-02 15:02:54 -0500

    added tests

--- review-day.py
+++ review-day.py
@@ -1,3 +1,5 @@
+import unittest
+
 def print_every_other(given_str):
     even = []
     odd = []
@@ -6,8 +8,47 @@ def print_every_other(given_str):
             even.append(char)
         else:
             odd.append(char) 
-    new_even = " ".join(even)
-    new_odd = " ".join(odd)
-    print(new_even + new_odd)
+    new_even = "".join(even)
+    new_odd = "".join(odd)
+    return f"{new_even} {new_odd}"
 
-print_every_other("Hacker")
+# Tests
+
+class Tests(unittest.TestCase):
+
+    def test_one_word(self):
+        given_str = "apple"
+        result = print_every_other(given_str)
+        expected = "ape pl"
+        self.assertEqual(result, expected)
+
+    def test_no_word(self):
+        given_str = ""
+        result = print_every_other(given_str)
+        expected = " "
+        self.assertEqual(result, expected)
+
+    def test_one_char(self):
+        given_str = "a"
+        result = print_every_other(given_str)
+        expected = "a "
+        self.assertEqual(result, expected)
+
+unittest.main(verbosity=2)
+
+
+#Hacker rank solution online suggestion
+# num_test_cases = int(input())
+
+# for i in range(num_test_cases):
+#     test_string = input()
+#     even_idx_char = ''
+#     odd_idx_char = ''
+
+#     for j in range(len(test_string)):
+#         if j % 2 == 0:
+#             even_idx_char += test_string[j]
+#         else:
+#             odd_idx_char += test_string[j]
+
+#     print(f"{even_idx_char} {odd_idx_char}")
