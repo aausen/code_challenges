fded8120345db41612290af9bf52340f784ea6e4
Annie Ausen <ausen.annie@gmail.com>
2021-08-10 14:28:46 -0500

    finished challenge

--- word-cloud.py
+++ word-cloud.py
@@ -1,35 +1,62 @@
-class WordCloudData(object):
+class WordCloudData:
 
     def __init__(self, input_string):
-        given_phrase = input_string.isalpha()
+        self.words_to_counts = {}
+        self.populate_words_to_counts(input_string)
 
-        
-
-        self.words_to_count = {}
-
-    def split_words(input_string):
-        words = []
+    def populate_words_to_counts(self, input_string):
         current_word_start_index = 0
         current_word_length = 0
-
+        
         for i, char in enumerate(input_string):
-            if char.isalpha():
+            if i == len(input_string) - 1:
+                if char.isalpha():
+                    current_word_length += 1
+                if current_word_length > 0:
+                    current_word = input_string[current_word_start_index: 
+                        current_word_start_index + current_word_length]
+                    self.add_word_to_dict(current_word)
+
+            elif char == " " or char == "\u2014":
+                if current_word_length > 0:
+                    current_word = input_string[current_word_start_index: 
+                        current_word_start_index + current_word_length]
+                    self.add_word_to_dict(current_word)
+                    current_word_length = 0
+
+            elif char == ".":
+                if i < len(input_string) - 1 and input_string[i + 1] ==".":
+                    if current_word_length > 0:
+                        current_word = input_string[current_word_start_index:
+                            current_word_start_index + current_word_length]
+                        self.add_word_to_dict(current_word)
+                        current_word_length = 0
+
+            elif char.isalpha() or char == "\'":
                 if current_word_length == 0:
                     current_word_start_index = i
                 current_word_length += 1
 
-            else:
-                word = input_string[current_word_start_index:
-                    current_word_start_index + current_word_length]
-                words.append(word)
-                current_word_length = 0
+            elif char == "-":
+                if i > 0 and input_string[i - 1].isalpha() and input_string[i + 1].isalpha():
+                    current_word_length += 1
+                else:
+                    if current_word_length > 0:
+                        current_word = input_string[current_word_start_index:
+                            current_word_start_index + current_word_length]
+                        self.add_word_to_dict(current_word)
+                        current_word_length = 0
 
-        return words
+    def add_word_to_dict(self, word):
+        if word in self.words_to_counts:
+            self.words_to_counts += 1
 
-    words_to_counts = {}
+        elif word.lower() in self.words_to_counts:
+            self.words_to_counts[word.lower()] += 1
 
-    def add_word_to_dict(word):
-        if word in words_to_counts:
-            words_to_counts[word] += 1
+        elif word.capitalize() in self.words_to_counts:
+            self.words_to_counts[word] += self.words_to_counts[word.capitalize()]
+            del self.words_to_counts[word.capitalize()]
+
         else:
-            words_to_count[word] = 1
+            self.words_to_counts[word] = 1
