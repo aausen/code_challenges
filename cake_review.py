068fbc4e4a1d5a4de8fbbf1671cddf22898c68af
Annie Ausen <ausen.annie@gmail.com>
2021-08-27 09:56:32 -0500

    review of fisher-yates shuffle

--- cake_review.py
+++ cake_review.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 ## Reverse string in place 8/16
 def reverse(list_of_char):
@@ -186,5 +187,25 @@ class Test(unittest.TestCase):
         expected = [(1, 9)]
         self.assertEqual(result, expected)
 
+# shuffle in place 
+
+def get_random(floor, ceiling):
+    return randrange(floor, ceiling + 1)
+
+def shuffle(the_list):
+    if len(the_list) <= 1:
+        return the_list
+
+    last_index_in_the_list = len(the_list) - 1
+
+    for index_we_are_choosing_for in range(0, len(the_list) - 1):
+        random_choice_index = get_random(index_we_are_choosing_for, last_index_in_the_list)
+
+        if random_choice_index != index_we_are_choosing_for:
+            the_list[index_we_are_choosing_for], the_list[random_choice_index] = \
+                the_list[random_choice_index], the_list[index_we_are_choosing_for]
+
+
+['duck', 'cat', 'dog', 'fish']
 
 unittest.main(verbosity=2)
