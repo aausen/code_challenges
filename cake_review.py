0bce3fefd723fe127d5a4a9f23ce1770bcc3d1d4
Annie Ausen <ausen.annie@gmail.com>
2021-09-10 15:56:39 -0500

    finish find repeat

--- cake_review.py
+++ cake_review.py
@@ -263,4 +263,35 @@ def sort_scores(unsorted_scores, highest_possible_score):
 
     return sorted_scores
 
+# find a duplicate
+
+def find_repeat(numbers):
+    floor = 1
+    ceiling = len(numbers) - 1
+
+    while floor < ceiling:
+        midpoint = floor + ((ceiling - floor) // 2)
+        lower_range_floor, lower_range_ceiling = floor, midpoint
+        upper_range_floor, upper_range_ceiling = midpoint+1, ceiling
+
+        items_in_lower_range = 0
+
+        for item in numbers:
+            if item >= lower_range_floor and item <= lower_range_ceiling:
+                items_in_lower_range += 1
+
+        distinct_possible_integers_in_lower_range = (
+            lower_range_ceiling
+            - lower_range_floor
+            + 1
+        )
+
+        if items_in_lower_range >= distinct_possible_integers_in_lower_range:
+            floor, ceiling = lower_range_floor, lower_range_ceiling
+
+        else:
+            floor, ceiling = upper_range_floor, upper_range_ceiling
+
+    return floor
+
 unittest.main(verbosity=2)
