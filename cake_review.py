1b636ac5dec4e89eb52016b09cdb510954799adf
Annie Ausen <ausen.annie@gmail.com>
2021-10-01 16:38:04 -0500

    finish max_profit

--- cake_review.py
+++ cake_review.py
@@ -310,4 +310,21 @@ def merge_meetings(meetings):
             merged_meetings.append((current_start, current_end))
     return merged_meetings
 
+
+# apple stocks
+def get_max_profit(stock_prices):
+    if len(stock_prices) < 2:
+        raise ValueError("Getting a profit requires at least two prices.")
+
+    min_price = stock_prices[0]
+    max_profit = stock_prices[1] - stock_prices[0]
+
+    for current_time in range(1, len(stock_prices)):
+        current_price = stock_prices[current_time]
+        potential_profit = current_price - min_price
+        max_profit = max(max_profit, potential_profit)
+        min_price = min(min_price, current_price) 
+
+    return max_profit
+
 unittest.main(verbosity=2)
