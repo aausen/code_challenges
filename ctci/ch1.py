e67dfdbcc0a369b676572021d4c200728c874f89
Annie Ausen <ausen.annie@gmail.com>
2021-10-20 16:50:14 -0500

    finish 1.5

--- ctci/ch1.py
+++ ctci/ch1.py
@@ -44,7 +44,59 @@ def is_palendrome_permutaion(stringA):
 
     return len(unpaired_char) <= 1
 
+#____________________________1.5______________________________#
+def one_away(str1, str2):
+    len_difference = len(str1) - len(str2)
+    if len_difference > 1:
+        return False
+    elif len_difference == 0:
+        diffrence = 0
+        for i in range(len(str1)):
+            if str1[i] != str2[i]:
+                diffrence += 1
+                if diffrence > 1:
+                    return False
+        return True
+    else:
+        if abs(len(str1) > len(str2)):
+            longer, shorter = str1, str2
+        else:
+            longer, shorter = str2, str1
+        move = 0
+        for i in range(len(shorter)):
+            if shorter[i] != longer[i + move]:
+                if move or (shorter[i] != longer[i + 1]):
+                    return False
+                move = 1
+        return True
 
+#_____________________1.6________________________#
+def string_compression(string1):
+    # count
+    # new string
+    # loop through string starting at 1
+    # if previous char is the same as current, add to count
+    # when char changes, add char[i - 1] with count to new string
+    # reset the count
+    # compare if len on new string is less than given string
+    # if so return new string
+    # otherwise return original string
+
+    count = 0
+    new_string = ""
+
+    for i in range(len(string1)):
+        if string1[i] == string1[i - 1]:
+            count += 0
+        else:
+            new_string = new_string + string[i - 1] + count
+            count = 0
+    if len(new_string) < string1:
+        return new_string
+    else:
+        return string1
+
+# needs to be fixed!
 
 class Tests(unittest.TestCase):
 
@@ -119,6 +171,27 @@ class Tests(unittest.TestCase):
     
     def test_is_not_palendrome(self):
         actual = is_palendrome_permutaion("this is")
+        expected = False
+        self.assertEqual(actual, expected)
+
+#_________________1.5_____________________________#
+    def test_remove_one_letter(self):
+        actual = one_away("pale", "ple")
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_insert_one_letter(self):
+        actual = one_away("pale", "pales")
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_replace_one_letter(self):
+        actual = one_away("bale", "pale")
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_not_one_away(self):
+        actual = one_away("bake", "pale")
         expected = False
         self.assertEqual(actual, expected)
 
