af8dd5efa5883898c69927b46d9a65556f5080ac
Annie Ausen <ausen.annie@gmail.com>
2021-11-08 11:52:37 -0600

    added comments

--- hbchallenges/largestsum.py
+++ hbchallenges/largestsum.py
@@ -38,22 +38,33 @@ than pick nothing!):
 
 def largest_sum(nums):
     """Find subsequence with largest sum."""
-    largest_sum_list = [nums[0]]
+    best_sum = 0
+    start_of_best = 0
+    end_of_best = -1
 
-    if sum(nums) < 0:
-        return []
-    for num in nums:
-        if (sum(largest_sum_list) + num) > sum(largest_sum_list):
-            largest_sum_list.append(num)
-        else:
-            largest_sum_list.pop(0)
-            if (sum(largest_sum_list) + num) > sum(largest_sum_list):
-                largest_sum_list.append(num)
+    current_sum = 0
+    start_of_current = 0
 
-    return largest_sum_list
+    for i, n in enumerate(nums):
+        current_sum += n
+
+        if current_sum > best_sum:
+            best_sum = current_sum
+            start_of_best = start_of_current
+            end_of_best = i
+
+        if current_sum <= 0:
+            start_of_current = i + 1
+            current_sum = 0
     
+    return nums[start_of_best:end_of_best + 1]
 
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
         print("\n*** ALL TESTS PASSED. YOU HANDLED THIS SUMMARILY!\n")
+
+# for this problem it was best to keep track of best sum as well as the indicies for the
+# beginning and ending of the best sum. When we looped through the list, we could compare
+# the best so far to the current sum. If the current sum was negative, we reset the variables
+# we return the list using list splicing from the index of the best start to the index of the best end + 1
